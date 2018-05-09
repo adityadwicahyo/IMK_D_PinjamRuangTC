f9358e9d4308d6a0b1dfcb76650ef4f25d74b167
Aditya Dwicahyo <adityadwicahyo21@gmail.com>
2018-05-09 12:24:41 +0700

    Tambah halaman home.

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+Beranda
+@endsection
+
+@section('content')
+	<img style="margin-left: 300px; margin-top: 100px" src="{{url('images/house.png')}}">
+@endsection
